0ef71bcc2413e8798d6bbdef4cd724ff9e0e91ec
Anderson <fuone.dev@gmail.com>
2026-07-06 17:59:36 -0300

    Create YapiScritp3.js
    Create

---
+++ YapiScritp3.js
@@ -0,0 +1,7 @@
+/*************************
+*  YAPISCRIPT 3.0
+* Framework JavaScript. Esse arquivo é parte do framwork Yapi
+* Edição 07/2026
+* Dev: Fuone
+* fuone.dev@gmaill.com
+**************************/
